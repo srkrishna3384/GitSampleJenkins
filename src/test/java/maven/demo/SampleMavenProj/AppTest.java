220563c36aa53124ef24869060379ab921367272
prashanthi1122 <pressi.563@gmail.com>
2019-12-15 08:53:16 +0530

    New project

---
+++ src/test/java/maven/demo/SampleMavenProj/AppTest.java
@@ -0,0 +1,18 @@
+package maven.demo.SampleMavenProj;
+
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+      //  assertTrue( true );
+    }
+}
---
+++ src/test/java/maven/demo/SampleMavenProj/Demo.java
@@ -0,0 +1,13 @@
+package maven.demo.SampleMavenProj;
+
+import org.testng.annotations.Test;
+
+
+
+public class Demo {
+	@Test
+	public static void test(){
+		System.out.println("sample maven project");
+	}
+
+}
