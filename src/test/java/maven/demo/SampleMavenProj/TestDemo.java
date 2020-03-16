cbc57f7e8da612fe3ceb6340c35439ce41f5777e
srkrishna3384 <srkrishna3384@gmail.com>
2020-03-16 21:26:41 +0530

    Launch browse

--- src/test/java/maven/demo/SampleMavenProj/TestDemo.java
+++ src/test/java/maven/demo/SampleMavenProj/TestDemo.java
@@ -1,7 +1,17 @@
 package maven.demo.SampleMavenProj;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
 public class TestDemo {
-public static void main(String[] args) {
+	@Test
+public static void m1() {
 	System.out.println("TestDemo class");
+	System.setProperty("webdriver.chrome.driver", "D:\\SampleMavenProj\\Drivers\\chromedriver.exe");
+	WebDriver driver = new ChromeDriver();
+	System.out.println("Browser launched successfully.........");
+	driver.get("https://www.gmail.com");
+	System.out.println("Navigated Gmail Page...........");
 }
 }
